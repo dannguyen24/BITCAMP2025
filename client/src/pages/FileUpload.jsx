672fdbe8c4a950f6c45a7a7ac1a0e6e1296e44f1
Dan Nguyen <danuyennguyen2412005@gmail.com>
2025-04-12 01:05:40 -0400

    Test sending file back to the server

---
+++ client/src/pages/FileUpload.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function FileUpload() {
+  const [file, setFile] = useState(null);
+
+  const handleChange = (e) => {
+    setFile(e.target.files[0]); // Get the file the user picked
+  };
+
+  const handleUpload = async () => {
+    if (!file) return;
+
+    const formData = new FormData(); // used to send files via HTTP
+    formData.append('file', file); // match this 'file' to Flask's request.files['file']
+
+    try {
+      const res = await axios.post('http://localhost:5000/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      alert('File uploaded: ' + res.data);
+    } catch (err) {
+      alert('Upload failed 😢');
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Upload File</h2>
+      <input type="file" onChange={handleChange} />
+      <button onClick={handleUpload}>Upload</button>
+    </div>
+  );
+}
+
+export default FileUpload;
--- server/app.py
+++ server/app.py
@@ -8,7 +8,25 @@ CORS(app)
 def home():
     return {"msg": "Flask server is up"}
 
+@app.route('/upload', methods=['POST'])
+def upload_video():
+    file = request.files[''] # name field in the input tag from frontend
+    print(file)
+    
+   
 
+@app.route("/analyze", methods=["POST"])
+def analyze():
+    return "analyze"
+    
+def extractAudio():
+    pass
+
+def audioToText():
+    pass
+
+
+    
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
